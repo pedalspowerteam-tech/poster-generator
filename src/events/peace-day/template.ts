3a1614770565a1bbe620d5352ae9569883a4f285
Vishal-sarkar <vishalsarkar852@gmail.com>
2026-09-18 01:08:55 +0530

    peace day

---
+++ src/events/peace-day/template.ts
@@ -0,0 +1,15 @@
+import { TemplateConfig } from '../../types';
+
+export const template: TemplateConfig = {
+  id: 'peace-day',
+  name: 'International Peace Day',
+  primaryColor: '#01361e', // Deep Green
+  accentColor: '#C5A880',  // Gold Accent
+  textColor: '#01361e',
+  fontName: 'font-sans',
+  fontTitle: 'font-sans',
+  fontLabel: 'font-sans',
+  badgeType: 'lightning',
+  bgColor: '#FFFFFF',
+  borderColor: '#01361e',
+};
